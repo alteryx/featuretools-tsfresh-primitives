5e906545ad6db7d5e7f39fe3a263b0ef6bc8bb74
Jeff Hernandez <inboxjeff@outlook.com>
2019-04-02 18:11:20 -0500

    update install_requires

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.0.0',
     author='Feature Labs, Inc.',
     author_email='support@featurelabs.com',
-    install_requires=['tsfresh>=0.11.2'],
+    install_requires=['tsfresh>=0.11.2', 'featuretools>=0.7.0'],
     packages=['featuretools_tsfresh_primitives'],
     entry_points={
         'featuretools_primitives': [
