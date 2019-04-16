3db37769e48071fdd62b778ca6b1e426fe499b5e
Jeff Hernandez <inboxjeff@outlook.com>
2019-04-15 19:07:21 -0500

    update variance_larger_than_standard_deviation

--- featuretools_tsfresh_primitives/variance_larger_than_standard_deviation.py
+++ featuretools_tsfresh_primitives/variance_larger_than_standard_deviation.py
@@ -1,16 +1,17 @@
 from tsfresh.feature_extraction.feature_calculators import variance_larger_than_standard_deviation
 
 from featuretools.primitives import AggregationPrimitive
-from featuretools.variable_types import Numeric
+from featuretools.variable_types import Numeric, Boolean
 
 
 class VarianceLargerThanStandardDeviation(AggregationPrimitive):
     """
-    Returns the variance_larger_than_standard_deviation of x.
+    Boolean variable denoting if the variance of x is greater than its standard deviation. Is equal to variance of x
+    being larger than 1
     """
     name = "variance_larger_than_standard_deviation"
     input_types = [Numeric]
-    return_type = Numeric
+    return_type = Boolean
     stack_on_self = False
 
     def get_function(self):
