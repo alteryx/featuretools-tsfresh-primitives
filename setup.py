70faed012b0498444db8162fc52e222a767e7d43
Roy Wedge <rwedge@featurelabs.com>
2019-04-30 14:33:55 -0400

    use plugin entrypoint instead of primitives entrypoint

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     install_requires=['tsfresh>=0.11.2', 'featuretools>=0.7.0'],
     packages=['featuretools_tsfresh_primitives'],
     entry_points={
-        'featuretools_primitives': [
+        'featuretools_plugin': [
             'tsfresh = featuretools_tsfresh_primitives',
         ],
     },
