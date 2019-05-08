20f9148982174933091dddd3b106c95378b0aca0
Roy Wedge <rwedge@featurelabs.com>
2019-05-08 14:56:19 -0400

    update FftCoeficient docstring

--- featuretools_tsfresh_primitives/fft_coefficient.py
+++ featuretools_tsfresh_primitives/fft_coefficient.py
@@ -1,25 +1,27 @@
-from tsfresh.feature_extraction.feature_calculators import fft_coefficient
-
 from featuretools.primitives import AggregationPrimitive
 from featuretools.variable_types import Numeric
+from tsfresh.feature_extraction.feature_calculators import fft_coefficient
 
 
 class FftCoefficient(AggregationPrimitive):
-    """
-    Calculates the fourier coefficients of the one-dimensional discrete Fourier Transform for real input by fast
-    fourier transformation algorithm
+    """Calculates the fourier coefficients of the one-dimensional discrete
+    Fourier Transform for real input by fast fourier transformation algorithm
 
     .. math::
         A_k =  \\sum_{m=0}^{n-1} a_m \\exp \\left \\{ -2 \\pi i \\frac{m k}{n} \\right \\}, \\qquad k = 0,
         \\ldots , n-1.
 
-    The resulting coefficients will be complex, this feature calculator can return the real part (attr=="real"),
-    the imaginary part (attr=="imag), the absolute value (attr=""abs) and the angle in degrees (attr=="angle).
+    The resulting coefficients will be complex, this feature calculator can
+    return the real part (attr=="real"), the imaginary part (attr=="imag), the
+    absolute value (attr=""abs) and the angle in degrees (attr=="angle).
 
     Args:
         coeff (int) : The coefficient to return. Must be greater than or equal to zero.
         attr (str) : Controls which attribute is returned.
             Possible values are: ["real", "imag", "abs", "angle"]
+
+    Docstring source:
+    https://tsfresh.readthedocs.io/en/latest/api/tsfresh.feature_extraction.html#tsfresh.feature_extraction.feature_calculators.fft_coefficient
     """
     name = "fft_coefficient"
     input_types = [Numeric]
