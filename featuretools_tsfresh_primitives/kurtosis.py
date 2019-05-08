cec131927c59582061f60cae48848ecf85675e7c
Roy Wedge <rwedge@featurelabs.com>
2019-05-08 15:47:44 -0400

    update Kurtosis docstring

--- featuretools_tsfresh_primitives/kurtosis.py
+++ featuretools_tsfresh_primitives/kurtosis.py
@@ -1,12 +1,14 @@
-from tsfresh.feature_extraction.feature_calculators import kurtosis
-
 from featuretools.primitives import AggregationPrimitive
 from featuretools.variable_types import Numeric
+from tsfresh.feature_extraction.feature_calculators import kurtosis
 
 
 class Kurtosis(AggregationPrimitive):
-    """
-    Returns the kurtosis of x (calculated with the adjusted Fisher-Pearson standardized moment coefficient G2).
+    """Returns the kurtosis of x (calculated with the adjusted Fisher-Pearson
+    standardized moment coefficient G2).
+
+    Docstring source:
+    https://tsfresh.readthedocs.io/en/latest/api/tsfresh.feature_extraction.html#tsfresh.feature_extraction.feature_calculators.kurtosis
     """
     name = "kurtosis"
     input_types = [Numeric]
