d9ad8068ac4f3df07346d698f85bb51038cb8620
Roy Wedge <rwedge@featurelabs.com>
2019-05-08 17:55:03 -0400

    update NumberCwtPeaks docstring

--- featuretools_tsfresh_primitives/number_cwt_peaks.py
+++ featuretools_tsfresh_primitives/number_cwt_peaks.py
@@ -1,17 +1,19 @@
-from tsfresh.feature_extraction.feature_calculators import number_cwt_peaks
-
 from featuretools.primitives import AggregationPrimitive
 from featuretools.variable_types import Numeric
+from tsfresh.feature_extraction.feature_calculators import number_cwt_peaks
 
 
 class NumberCwtPeaks(AggregationPrimitive):
-    """
-    This feature calculator searches for different peaks in x. To do so, x is smoothed by a ricker wavelet and for
-    widths ranging from 1 to n. This feature calculator returns the number of peaks that occur at enough width scales
-    and with sufficiently high Signal-to-Noise-Ratio (SNR).
+    """This feature calculator searches for different peaks in x. To do so, x
+    is smoothed by a ricker wavelet and for widths ranging from 1 to n. This
+    feature calculator returns the number of peaks that occur at enough width
+    scales and with sufficiently high Signal-to-Noise-Ratio (SNR).
 
     Args:
         n (int) : Maximum width to consider.
+
+    Docstring source:
+    https://tsfresh.readthedocs.io/en/latest/api/tsfresh.feature_extraction.html#tsfresh.feature_extraction.feature_calculators.number_cwt_peaks
     """
     name = "number_cwt_peaks"
     input_types = [Numeric]
