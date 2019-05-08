d4a3059e27da55345c2e5d6be35bb7c209b80aec
Roy Wedge <rwedge@featurelabs.com>
2019-05-08 18:26:10 -0400

    update SumOfReoccurringValues docstring

--- featuretools_tsfresh_primitives/sum_of_reoccurring_values.py
+++ featuretools_tsfresh_primitives/sum_of_reoccurring_values.py
@@ -1,13 +1,14 @@
-from tsfresh.feature_extraction.feature_calculators import sum_of_reoccurring_values
-
 from featuretools.primitives import AggregationPrimitive
 from featuretools.variable_types import Numeric
+from tsfresh.feature_extraction.feature_calculators import sum_of_reoccurring_values
 
 
 class SumOfReoccurringValues(AggregationPrimitive):
-    """
-    Returns the sum of all values, that are present in the time series
-    more than once.
+    """Returns the sum of all values, that are present in the time series more
+    than once.
+
+    Docstring source:
+    https://tsfresh.readthedocs.io/en/latest/api/tsfresh.feature_extraction.html#tsfresh.feature_extraction.feature_calculators.sum_of_reoccurring_values
     """
     name = "sum_of_reoccurring_values"
     input_types = [Numeric]
