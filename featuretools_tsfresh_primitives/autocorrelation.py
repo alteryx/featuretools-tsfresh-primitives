fa38be4f6a52541940133f107c7515fe69d4ea2e
Roy Wedge <rwedge@featurelabs.com>
2019-05-08 14:08:45 -0400

    update Autocorrelation docstring

--- featuretools_tsfresh_primitives/autocorrelation.py
+++ featuretools_tsfresh_primitives/autocorrelation.py
@@ -1,15 +1,16 @@
-from tsfresh.feature_extraction.feature_calculators import autocorrelation
-
 from featuretools.primitives import AggregationPrimitive
 from featuretools.variable_types import Numeric
+from tsfresh.feature_extraction.feature_calculators import autocorrelation
 
 
 class Autocorrelation(AggregationPrimitive):
-    """
-    Calculates the autocorrelation of the specified lag.
+    """Calculates the autocorrelation of the specified lag.
 
     Args:
         lag (int) : The lag.
+
+    Docstring source:
+    https://tsfresh.readthedocs.io/en/latest/api/tsfresh.feature_extraction.html#tsfresh.feature_extraction.feature_calculators.autocorrelation
     """
     name = "autocorrelation"
     input_types = [Numeric]
