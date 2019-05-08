49a8fa45ac31b8c9b8c894780ed4fa28ebdde01e
Roy Wedge <rwedge@featurelabs.com>
2019-05-08 15:51:24 -0400

    update LastLocationOfMaximum docstring

--- featuretools_tsfresh_primitives/last_location_of_maximum.py
+++ featuretools_tsfresh_primitives/last_location_of_maximum.py
@@ -1,13 +1,14 @@
-from tsfresh.feature_extraction.feature_calculators import last_location_of_maximum
-
 from featuretools.primitives import AggregationPrimitive
 from featuretools.variable_types import Numeric
+from tsfresh.feature_extraction.feature_calculators import last_location_of_maximum
 
 
 class LastLocationOfMaximum(AggregationPrimitive):
-    """
-    Returns the relative last location of the maximum value of x.
+    """Returns the relative last location of the maximum value of x.
     The position is calculated relatively to the length of x.
+
+    Docstring source:
+    https://tsfresh.readthedocs.io/en/latest/api/tsfresh.feature_extraction.html#tsfresh.feature_extraction.feature_calculators.last_location_of_maximum
     """
     name = "last_location_of_maximum"
     input_types = [Numeric]
