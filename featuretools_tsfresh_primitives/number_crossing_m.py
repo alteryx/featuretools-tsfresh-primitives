0296ebd9b4b331f92d886fdad403160d24a2b3e8
Roy Wedge <rwedge@featurelabs.com>
2019-05-08 17:51:50 -0400

    update NumberCrossingM docstring

--- featuretools_tsfresh_primitives/number_crossing_m.py
+++ featuretools_tsfresh_primitives/number_crossing_m.py
@@ -1,17 +1,19 @@
-from tsfresh.feature_extraction.feature_calculators import number_crossing_m
-
 from featuretools.primitives import AggregationPrimitive
 from featuretools.variable_types import Numeric
+from tsfresh.feature_extraction.feature_calculators import number_crossing_m
 
 
 class NumberCrossingM(AggregationPrimitive):
-    """
-    Calculates the number of crossings of x on m. A crossing is defined as two sequential values where the first value
-    is lower than m and the next is greater, or vice-versa. If you set m to zero, you will get the number of zero
-    crossings.
+    """Calculates the number of crossings of x on m. A crossing is defined as
+    two sequential values where the first value is lower than m and the next is
+    greater, or vice-versa. If you set m to zero, you will get the number of
+    zero crossings.
 
     Args:
         m (float) : The threshold for the crossing.
+
+    Docstring source:
+    https://tsfresh.readthedocs.io/en/latest/api/tsfresh.feature_extraction.html#tsfresh.feature_extraction.feature_calculators.number_crossing_m
     """
     name = "number_crossing_m"
     input_types = [Numeric]
