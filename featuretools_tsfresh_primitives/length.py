ca4012dd5cb9eaefee95c93df3c202e78aa88ad8
Roy Wedge <rwedge@featurelabs.com>
2019-05-08 17:01:31 -0400

    update Length docstring

--- featuretools_tsfresh_primitives/length.py
+++ featuretools_tsfresh_primitives/length.py
@@ -1,12 +1,13 @@
-from tsfresh.feature_extraction.feature_calculators import length
-
 from featuretools.primitives import AggregationPrimitive
 from featuretools.variable_types import Numeric
+from tsfresh.feature_extraction.feature_calculators import length
 
 
 class Length(AggregationPrimitive):
-    """
-    Returns the length of x.
+    """Returns the length of x.
+
+    Docstring source:
+    https://tsfresh.readthedocs.io/en/latest/api/tsfresh.feature_extraction.html#tsfresh.feature_extraction.feature_calculators.length
     """
     name = "length"
     input_types = [Numeric]
