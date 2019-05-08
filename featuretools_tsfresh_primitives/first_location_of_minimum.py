13188ce2b7440f56b1e440f809f4bd2f6aa1beda
Roy Wedge <rwedge@featurelabs.com>
2019-05-08 15:03:04 -0400

    update FirstLocationofMimimum docstring

--- featuretools_tsfresh_primitives/first_location_of_minimum.py
+++ featuretools_tsfresh_primitives/first_location_of_minimum.py
@@ -1,12 +1,14 @@
-from tsfresh.feature_extraction.feature_calculators import first_location_of_minimum
-
 from featuretools.primitives import AggregationPrimitive
 from featuretools.variable_types import Numeric
+from tsfresh.feature_extraction.feature_calculators import first_location_of_minimum
 
 
 class FirstLocationOfMinimum(AggregationPrimitive):
-    """
-    Returns the first location of the minimal value of x. The position is calculated relatively to the length of x.
+    """Returns the first location of the minimal value of x. The position is
+    calculated relatively to the length of x.
+
+    Docstring source:
+    https://tsfresh.readthedocs.io/en/latest/api/tsfresh.feature_extraction.html#tsfresh.feature_extraction.feature_calculators.first_location_of_minimum
     """
     name = "first_location_of_minimum"
     input_types = [Numeric]
