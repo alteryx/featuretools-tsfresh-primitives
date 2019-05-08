eb0b8aee37ee6a60d5b56dc5b4848a5140cf77a2
Roy Wedge <rwedge@featurelabs.com>
2019-05-08 18:23:46 -0400

    update SpktWelchDensity docstring

--- featuretools_tsfresh_primitives/spkt_welch_density.py
+++ featuretools_tsfresh_primitives/spkt_welch_density.py
@@ -1,16 +1,18 @@
-from tsfresh.feature_extraction.feature_calculators import spkt_welch_density
-
 from featuretools.primitives import AggregationPrimitive
 from featuretools.variable_types import Numeric
+from tsfresh.feature_extraction.feature_calculators import spkt_welch_density
 
 
 class SpktWelchDensity(AggregationPrimitive):
-    """
-    This feature calculator estimates the cross power spectral density of the time series at different frequencies.
-    To do so, the time series is first shifted from the time domain to the frequency domain.
+    """This feature calculator estimates the cross power spectral density of
+    the time series at different frequencies. To do so, the time series is
+    first shifted from the time domain to the frequency domain.
 
     Args:
         coeff (int) : Value of coefficient.
+
+    Docstring source:
+    https://tsfresh.readthedocs.io/en/latest/api/tsfresh.feature_extraction.html#tsfresh.feature_extraction.feature_calculators.spkt_welch_density
     """
     name = "spkt_welch_density"
     input_types = [Numeric]
