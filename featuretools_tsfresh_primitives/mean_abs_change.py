93df7a5a6cfd280bad3795f801d29759437fca30
Roy Wedge <rwedge@featurelabs.com>
2019-05-08 17:34:45 -0400

    update MeanAbsChange docstring

--- featuretools_tsfresh_primitives/mean_abs_change.py
+++ featuretools_tsfresh_primitives/mean_abs_change.py
@@ -1,16 +1,18 @@
-from tsfresh.feature_extraction.feature_calculators import mean_abs_change
-
 from featuretools.primitives import AggregationPrimitive
 from featuretools.variable_types import Numeric
+from tsfresh.feature_extraction.feature_calculators import mean_abs_change
 
 
 class MeanAbsChange(AggregationPrimitive):
-    """
-    Returns the mean over the absolute differences between subsequent time series values which is
+    """Returns the mean over the absolute differences between subsequent time
+    series values which is
 
     .. math::
 
         \\frac{1}{n} \\sum_{i=1,\\ldots, n-1} | x_{i+1} - x_{i}|
+
+    Docstring source:
+    https://tsfresh.readthedocs.io/en/latest/api/tsfresh.feature_extraction.html#tsfresh.feature_extraction.feature_calculators.mean_abs_change
     """
     name = "mean_abs_change"
     input_types = [Numeric]
