3d163c2e9b300be846421af44210a6431d80aeee
Roy Wedge <rwedge@featurelabs.com>
2019-05-08 18:06:07 -0400

    update PercentageOfReocurringDatapointsToAllDatapoints docstring

--- featuretools_tsfresh_primitives/percentage_of_reoccurring_datapoints_to_all_datapoints.py
+++ featuretools_tsfresh_primitives/percentage_of_reoccurring_datapoints_to_all_datapoints.py
@@ -1,18 +1,19 @@
-from tsfresh.feature_extraction.feature_calculators import percentage_of_reoccurring_datapoints_to_all_datapoints
-
 from featuretools.primitives import AggregationPrimitive
 from featuretools.variable_types import Numeric
+from tsfresh.feature_extraction.feature_calculators import percentage_of_reoccurring_datapoints_to_all_datapoints
 
 
 class PercentageOfReoccurringDatapointsToAllDatapoints(AggregationPrimitive):
-    """
-    Returns the percentage of unique values, that are present in the time series
-    more than once.
+    """Returns the percentage of unique values, that are present in the time
+    series more than once.
 
         len(different values occurring more than once) / len(different values)
 
     This means the percentage is normalized to the number of unique values,
     in contrast to the percentage_of_reoccurring_values_to_all_values.
+
+    Docstring source:
+    https://tsfresh.readthedocs.io/en/latest/api/tsfresh.feature_extraction.html#tsfresh.feature_extraction.feature_calculators.percentage_of_reoccurring_datapoints_to_all_datapoints
     """
     name = "percentage_of_reoccurring_datapoints_to_all_datapoints"
     input_types = [Numeric]
