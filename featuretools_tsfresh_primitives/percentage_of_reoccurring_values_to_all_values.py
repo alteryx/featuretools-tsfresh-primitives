f5b1fbcaa54f3cb57f9bcfc333dccde354db9f3d
Roy Wedge <rwedge@featurelabs.com>
2019-05-08 18:10:07 -0400

    update PercentageOfReoccurringValuesToAllValues docstring

--- featuretools_tsfresh_primitives/percentage_of_reoccurring_values_to_all_values.py
+++ featuretools_tsfresh_primitives/percentage_of_reoccurring_values_to_all_values.py
@@ -1,18 +1,20 @@
-from tsfresh.feature_extraction.feature_calculators import percentage_of_reoccurring_values_to_all_values
-
 from featuretools.primitives import AggregationPrimitive
 from featuretools.variable_types import Numeric
+from tsfresh.feature_extraction.feature_calculators import percentage_of_reoccurring_values_to_all_values
 
 
 class PercentageOfReoccurringValuesToAllValues(AggregationPrimitive):
-    """
-    Returns the ratio of unique values, that are present in the time series
+    """Returns the ratio of unique values, that are present in the time series
     more than once.
 
         # of data points occurring more than once / # of all data points
 
-    This means the ratio is normalized to the number of data points in the time series,
-    in contrast to the percentage_of_reoccurring_datapoints_to_all_datapoints.
+    This means the ratio is normalized to the number of data points in the time
+    series, in contrast to the
+    percentage_of_reoccurring_datapoints_to_all_datapoints.
+
+    Docstring source:
+    https://tsfresh.readthedocs.io/en/latest/api/tsfresh.feature_extraction.html#tsfresh.feature_extraction.feature_calculators.percentage_of_reoccurring_values_to_all_values
     """
     name = "percentage_of_reoccurring_values_to_all_values"
     input_types = [Numeric]
