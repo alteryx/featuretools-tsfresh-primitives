2d32da1a1b90fe86e0a4a9dc5b472267f44618d5
Roy Wedge <rwedge@featurelabs.com>
2019-05-08 12:20:20 -0400

    update AggLinearTrend docstring and primitive import order

--- featuretools_tsfresh_primitives/agg_linear_trend.py
+++ featuretools_tsfresh_primitives/agg_linear_trend.py
@@ -1,18 +1,23 @@
-from tsfresh.feature_extraction.feature_calculators import agg_linear_trend
-
 from featuretools.primitives import AggregationPrimitive
 from featuretools.variable_types import Numeric
+from tsfresh.feature_extraction.feature_calculators import agg_linear_trend
 
 
 class AggLinearTrend(AggregationPrimitive):
-    """
-    Calculates a linear least-squares regression for values of the time series that were aggregated over chunks versus the sequence from 0 up to the number of chunks minus one.
+    """Calculates a linear least-squares regression for values of the time
+    series that were aggregated over chunks versus the sequence from 0 up to
+    the number of chunks minus one.
 
     Args:
         attr (str) : Controls which of the characteristics are returned.
-            Possible extracted attributes are: ['pvalue', 'rvalue', 'intercept', 'slope', 'stderr'].
-        chunk_len (int) : Specifies how many time series values are in each chunk.
-        f_agg (str) : Name of the aggregator function. Possible values are: [ 'max', 'min' or , 'mean', 'median'].
+            Possible extracted attributes are: ['pvalue', 'rvalue',
+            'intercept', 'slope', 'stderr'].
+        chunk_len (int) : How many time series values are in each chunk.
+        f_agg (str) : Name of the aggregator function. Possible values are:
+            ['max', 'min' or , 'mean', 'median'].
+
+    Docstring source:
+    https://tsfresh.readthedocs.io/en/latest/api/tsfresh.feature_extraction.html#tsfresh.feature_extraction.feature_calculators.agg_linear_trend
     """
     name = "agg_linear_trend"
     input_types = [Numeric]
