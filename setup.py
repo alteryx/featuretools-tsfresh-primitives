832070a74a187f08656d18f511b87bd410c41f09
Roy Wedge <rwedge@featurelabs.com>
2019-05-10 14:34:17 -0400

    update version and license type in setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import find_packages, setup
 
 setup(
     name='featuretools_tsfresh_primitives',
-    version='0.0.0',
+    version='0.1.0',
     author='Feature Labs, Inc.',
     author_email='support@featurelabs.com',
+    license='MIT',
     install_requires=['tsfresh>=0.11.2', 'featuretools>=0.7.0'],
     packages=find_packages(),
     include_package_data=True,
