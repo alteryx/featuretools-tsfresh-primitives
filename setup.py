f252ad59c0ff3fd741df9a5c337ed0cfc2b424a7
Roy Wedge <rwedge@featurelabs.com>
2019-05-10 14:50:40 -0400

    Add package manifest (#7)
    * add manifest file and update setup.py accordingly
    
    * update version and license type in setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name='featuretools_tsfresh_primitives',
-    version='0.0.0',
+    version='0.1.0',
     author='Feature Labs, Inc.',
     author_email='support@featurelabs.com',
+    license='MIT',
     install_requires=['tsfresh>=0.11.2', 'featuretools>=0.7.0'],
-    packages=['featuretools_tsfresh_primitives'],
+    packages=find_packages(),
+    include_package_data=True,
     entry_points={
         'featuretools_plugin': [
             'tsfresh = featuretools_tsfresh_primitives',
