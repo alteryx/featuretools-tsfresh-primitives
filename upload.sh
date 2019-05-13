1040accd49213c294c2fc9348f39971f23321428
Roy Wedge <rwedge@featurelabs.com>
2019-05-13 17:42:15 -0400

    add upload script

---
+++ upload.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Clone featuretools-tsfresh-primitives repo
+git clone git@github.com:FeatureLabs/featuretools-tsfresh-primitives.git /home/circleci/featuretools-tsfresh-primitives
+# Checkout specified commit
+cd /home/circleci/featuretools-tsfresh-primitives
+git checkout "${1}"
+# Remove build artifacts
+rm -rf .eggs/ rm -rf dist/ rm -rf build/
+# Create distributions
+python setup.py sdist bdist_wheel
+# Install twine, module used to upload to pypi
+pip install --user twine
+# Upload to pypi or testpypi
+echo "Upoading to ${2:-pypi} . . ."
+python -m twine upload dist/* -r "${2:-pypi}"
