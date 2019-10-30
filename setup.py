6d17d236f20b9f75d34eb71e0a974e69af24ecb6
Jeff Hernandez <inboxjeff@outlook.com>
2019-10-30 12:21:39 -0500

    PyPI Readme (#24)

--- setup.py
+++ setup.py
@@ -5,8 +5,12 @@ setup(
     version='0.1.0',
     author='Feature Labs, Inc.',
     author_email='support@featurelabs.com',
+    description='TSFresh primitives for featuretools',
     license='MIT',
     install_requires=['tsfresh>=0.11.2', 'featuretools>=0.7.0'],
+    tests_require=open('test-requirements.txt').readlines(),
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     include_package_data=True,
     entry_points={
