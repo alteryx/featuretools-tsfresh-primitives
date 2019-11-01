f8402263972e0fd15e0ef392b3f35ba0338d3ff1
Jeff Hernandez <inboxjeff@outlook.com>
2019-11-01 17:34:52 -0500

    Fix Pandas Version (#27)

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email='support@featurelabs.com',
     description='TSFresh primitives for featuretools',
     license='MIT',
-    install_requires=['tsfresh>=0.11.2', 'featuretools>=0.7.0,<0.12.0'],
+    install_requires=['tsfresh>=0.11.2', 'featuretools>=0.7.0,<0.12.0', 'pandas>=0.23.0,<0.24.0'],
     tests_require=open('test-requirements.txt').readlines(),
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
