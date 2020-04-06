665fa65be428a542d328e9c4bcf2e05b5afe8dd2
Jeff Hernandez <12969559+jeff-hernandez@users.noreply.github.com>
2020-04-06 17:08:39 -0500

    count above

--- featuretools_tsfresh_primitives/__init__.py
+++ featuretools_tsfresh_primitives/__init__.py
@@ -11,6 +11,7 @@ from .primitives.binned_entropy import BinnedEntropy
 from .primitives.c3 import C3
 from .primitives.change_quantiles import ChangeQuantiles
 from .primitives.cid_ce import CidCe
+from .primitives.count_above import CountAbove
 from .primitives.count_above_mean import CountAboveMean
 from .primitives.count_below_mean import CountBelowMean
 from .primitives.cwt_coefficients import CwtCoefficients
