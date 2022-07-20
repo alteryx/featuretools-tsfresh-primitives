213b7ea1f20c58fc43d92d36af13af5db2d84bb4
Gaurav Sheni <gaurav.sheni@alteryx.com>
2022-07-20 17:45:04 -0400

    fix pre commit

--- featuretools_tsfresh_primitives/__init__.py
+++ featuretools_tsfresh_primitives/__init__.py
@@ -1,5 +1,8 @@
 # flake8: noqa
-from .primitives import *
-from .utils import comprehensive_fc_parameters, primitives_from_fc_settings
+from featuretools_tsfresh_primitives.primitives import *
+from featuretools_tsfresh_primitives.utils import (
+    comprehensive_fc_parameters,
+    primitives_from_fc_settings,
+)
 
 __version__ = "1.0.2"
--- featuretools_tsfresh_primitives/primitives/__init__.py
+++ featuretools_tsfresh_primitives/primitives/__init__.py
@@ -1,73 +1,125 @@
 # flake8: noqa
-from .abs_energy import AbsEnergy
-from .absolute_sum_of_changes import AbsoluteSumOfChanges
-from .agg_autocorrelation import AggAutocorrelation
-from .agg_linear_trend import AggLinearTrend
-from .approximate_entropy import ApproximateEntropy
-from .ar_coefficient import ArCoefficient
-from .augmented_dickey_fuller import AugmentedDickeyFuller
-from .autocorrelation import Autocorrelation
-from .binned_entropy import BinnedEntropy
-from .c3 import C3
-from .change_quantiles import ChangeQuantiles
-from .cid_ce import CidCe
-from .count_above import CountAbove
-from .count_above_mean import CountAboveMean
-from .count_below import CountBelow
-from .count_below_mean import CountBelowMean
-from .cwt_coefficients import CwtCoefficients
-from .energy_ratio_by_chunks import EnergyRatioByChunks
-from .fft_aggregated import FftAggregated
-from .fft_coefficient import FftCoefficient
-from .first_location_of_maximum import FirstLocationOfMaximum
-from .first_location_of_minimum import FirstLocationOfMinimum
-from .friedrich_coefficients import FriedrichCoefficients
-from .has_duplicate import HasDuplicate
-from .has_duplicate_max import HasDuplicateMax
-from .has_duplicate_min import HasDuplicateMin
-from .index_mass_quantile import IndexMassQuantile
-from .kurtosis import Kurtosis
-from .large_standard_deviation import LargeStandardDeviation
-from .last_location_of_maximum import LastLocationOfMaximum
-from .last_location_of_minimum import LastLocationOfMinimum
-from .length import Length
-from .linear_trend import LinearTrend
-from .linear_trend_timewise import LinearTrendTimewise
-from .longest_strike_above_mean import LongestStrikeAboveMean
-from .longest_strike_below_mean import LongestStrikeBelowMean
-from .max_langevin_fixed_point import MaxLangevinFixedPoint
-from .maximum import Maximum
-from .mean import Mean
-from .mean_abs_change import MeanAbsChange
-from .mean_change import MeanChange
-from .mean_second_derivative_central import MeanSecondDerivativeCentral
-from .median import Median
-from .minimum import Minimum
-from .number_crossing_m import NumberCrossingM
-from .number_cwt_peaks import NumberCwtPeaks
-from .number_peaks import NumberPeaks
-from .partial_autocorrelation import PartialAutocorrelation
-from .percentage_of_reoccurring_datapoints_to_all_datapoints import (
+from featuretools_tsfresh_primitives.primitives.abs_energy import AbsEnergy
+from featuretools_tsfresh_primitives.primitives.absolute_sum_of_changes import (
+    AbsoluteSumOfChanges,
+)
+from featuretools_tsfresh_primitives.primitives.agg_autocorrelation import (
+    AggAutocorrelation,
+)
+from featuretools_tsfresh_primitives.primitives.agg_linear_trend import AggLinearTrend
+from featuretools_tsfresh_primitives.primitives.approximate_entropy import (
+    ApproximateEntropy,
+)
+from featuretools_tsfresh_primitives.primitives.ar_coefficient import ArCoefficient
+from featuretools_tsfresh_primitives.primitives.augmented_dickey_fuller import (
+    AugmentedDickeyFuller,
+)
+from featuretools_tsfresh_primitives.primitives.autocorrelation import Autocorrelation
+from featuretools_tsfresh_primitives.primitives.binned_entropy import BinnedEntropy
+from featuretools_tsfresh_primitives.primitives.c3 import C3
+from featuretools_tsfresh_primitives.primitives.change_quantiles import ChangeQuantiles
+from featuretools_tsfresh_primitives.primitives.cid_ce import CidCe
+from featuretools_tsfresh_primitives.primitives.count_above import CountAbove
+from featuretools_tsfresh_primitives.primitives.count_above_mean import CountAboveMean
+from featuretools_tsfresh_primitives.primitives.count_below import CountBelow
+from featuretools_tsfresh_primitives.primitives.count_below_mean import CountBelowMean
+from featuretools_tsfresh_primitives.primitives.cwt_coefficients import CwtCoefficients
+from featuretools_tsfresh_primitives.primitives.energy_ratio_by_chunks import (
+    EnergyRatioByChunks,
+)
+from featuretools_tsfresh_primitives.primitives.fft_aggregated import FftAggregated
+from featuretools_tsfresh_primitives.primitives.fft_coefficient import FftCoefficient
+from featuretools_tsfresh_primitives.primitives.first_location_of_maximum import (
+    FirstLocationOfMaximum,
+)
+from featuretools_tsfresh_primitives.primitives.first_location_of_minimum import (
+    FirstLocationOfMinimum,
+)
+from featuretools_tsfresh_primitives.primitives.friedrich_coefficients import (
+    FriedrichCoefficients,
+)
+from featuretools_tsfresh_primitives.primitives.has_duplicate import HasDuplicate
+from featuretools_tsfresh_primitives.primitives.has_duplicate_max import HasDuplicateMax
+from featuretools_tsfresh_primitives.primitives.has_duplicate_min import HasDuplicateMin
+from featuretools_tsfresh_primitives.primitives.index_mass_quantile import (
+    IndexMassQuantile,
+)
+from featuretools_tsfresh_primitives.primitives.kurtosis import Kurtosis
+from featuretools_tsfresh_primitives.primitives.large_standard_deviation import (
+    LargeStandardDeviation,
+)
+from featuretools_tsfresh_primitives.primitives.last_location_of_maximum import (
+    LastLocationOfMaximum,
+)
+from featuretools_tsfresh_primitives.primitives.last_location_of_minimum import (
+    LastLocationOfMinimum,
+)
+from featuretools_tsfresh_primitives.primitives.length import Length
+from featuretools_tsfresh_primitives.primitives.linear_trend import LinearTrend
+from featuretools_tsfresh_primitives.primitives.linear_trend_timewise import (
+    LinearTrendTimewise,
+)
+from featuretools_tsfresh_primitives.primitives.longest_strike_above_mean import (
+    LongestStrikeAboveMean,
+)
+from featuretools_tsfresh_primitives.primitives.longest_strike_below_mean import (
+    LongestStrikeBelowMean,
+)
+from featuretools_tsfresh_primitives.primitives.max_langevin_fixed_point import (
+    MaxLangevinFixedPoint,
+)
+from featuretools_tsfresh_primitives.primitives.maximum import Maximum
+from featuretools_tsfresh_primitives.primitives.mean import Mean
+from featuretools_tsfresh_primitives.primitives.mean_abs_change import MeanAbsChange
+from featuretools_tsfresh_primitives.primitives.mean_change import MeanChange
+from featuretools_tsfresh_primitives.primitives.mean_second_derivative_central import (
+    MeanSecondDerivativeCentral,
+)
+from featuretools_tsfresh_primitives.primitives.median import Median
+from featuretools_tsfresh_primitives.primitives.minimum import Minimum
+from featuretools_tsfresh_primitives.primitives.number_crossing_m import NumberCrossingM
+from featuretools_tsfresh_primitives.primitives.number_cwt_peaks import NumberCwtPeaks
+from featuretools_tsfresh_primitives.primitives.number_peaks import NumberPeaks
+from featuretools_tsfresh_primitives.primitives.partial_autocorrelation import (
+    PartialAutocorrelation,
+)
+from featuretools_tsfresh_primitives.primitives.percentage_of_reoccurring_datapoints_to_all_datapoints import (
     PercentageOfReoccurringDatapointsToAllDatapoints,
 )
-from .percentage_of_reoccurring_values_to_all_values import (
+from featuretools_tsfresh_primitives.primitives.percentage_of_reoccurring_values_to_all_values import (
     PercentageOfReoccurringValuesToAllValues,
 )
-from .quantile import Quantile
-from .range_count import RangeCount
-from .ratio_beyond_r_sigma import RatioBeyondRSigma
-from .ratio_value_number_to_time_series_length import RatioValueNumberToTimeSeriesLength
-from .sample_entropy import SampleEntropy
-from .skewness import Skewness
-from .spkt_welch_density import SpktWelchDensity
-from .standard_deviation import StandardDeviation
-from .sum_of_reoccurring_data_points import SumOfReoccurringDataPoints
-from .sum_of_reoccurring_values import SumOfReoccurringValues
-from .sum_values import SumValues
-from .symmetry_looking import SymmetryLooking
-from .time_reversal_asymmetry_statistic import TimeReversalAsymmetryStatistic
-from .value_count import ValueCount
-from .variance import Variance
-from .variance_larger_than_standard_deviation import VarianceLargerThanStandardDeviation
+from featuretools_tsfresh_primitives.primitives.quantile import Quantile
+from featuretools_tsfresh_primitives.primitives.range_count import RangeCount
+from featuretools_tsfresh_primitives.primitives.ratio_beyond_r_sigma import (
+    RatioBeyondRSigma,
+)
+from featuretools_tsfresh_primitives.primitives.ratio_value_number_to_time_series_length import (
+    RatioValueNumberToTimeSeriesLength,
+)
+from featuretools_tsfresh_primitives.primitives.sample_entropy import SampleEntropy
+from featuretools_tsfresh_primitives.primitives.skewness import Skewness
+from featuretools_tsfresh_primitives.primitives.spkt_welch_density import (
+    SpktWelchDensity,
+)
+from featuretools_tsfresh_primitives.primitives.standard_deviation import (
+    StandardDeviation,
+)
+from featuretools_tsfresh_primitives.primitives.sum_of_reoccurring_data_points import (
+    SumOfReoccurringDataPoints,
+)
+from featuretools_tsfresh_primitives.primitives.sum_of_reoccurring_values import (
+    SumOfReoccurringValues,
+)
+from featuretools_tsfresh_primitives.primitives.sum_values import SumValues
+from featuretools_tsfresh_primitives.primitives.symmetry_looking import SymmetryLooking
+from featuretools_tsfresh_primitives.primitives.time_reversal_asymmetry_statistic import (
+    TimeReversalAsymmetryStatistic,
+)
+from featuretools_tsfresh_primitives.primitives.value_count import ValueCount
+from featuretools_tsfresh_primitives.primitives.variance import Variance
+from featuretools_tsfresh_primitives.primitives.variance_larger_than_standard_deviation import (
+    VarianceLargerThanStandardDeviation,
+)
 
 SUPPORTED_PRIMITIVES = [value for value in locals().values() if isinstance(value, type)]
